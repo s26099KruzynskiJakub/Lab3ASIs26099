53aa6ea55bccd13f99ed6bcfac16d5899bf389c7
s26099KruzynskiJakub <126682305+s26099KruzynskiJakub@users.noreply.github.com>
2024-10-23 12:38:17 +0200

    Update czyszczenie_porównywanieModeli.py

--- asi3Project/czyszczenie_porównywanieModeli.py
+++ asi3Project/czyszczenie_porównywanieModeli.py
@@ -173,3 +173,5 @@ doc.add_paragraph(f'W niniejszej pracy wykonano pełną analizę danych, w tym i
                   f'Zastosowano kilka modeli predykcyjnych, spośród których najlepszy okazał się model "{best_model_name}" z najniższym błędem średniokwadratowym (MSE): {best_mse:.4f}. '
                   'Dokument ten zawiera zarówno szczegóły dotyczące poszczególnych zmiennych, jak i oceny porównywanych modeli. '
                   'W razie potrzeby można przeprowadzić dodatkową optymalizację, aby jeszcze bardziej poprawić jakość modelu.')
+doc.save(nazwaWord)
+print(f'Dokument zapisany jako {nazwaWord}.')
